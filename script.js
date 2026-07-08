cb1ad0254c99b0ec49cb5b8f94282db9a091207d
Valentina BM <valentina15bermo@gmail.com>
2026-07-08 17:01:06 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// This file contains the logic behinf our garderner/landscaper app!
+
+// establish variables
+let player;
+let lawn;
+let cut; 
+let tools; //hands, scissors, push mowers, army of goats, army o
+let money = 5;
+
+if(money >= 100){
+    console.log("Your current tool are scissors")
+} else {
+    console.log("continue using your hands until you get your money")
+}
